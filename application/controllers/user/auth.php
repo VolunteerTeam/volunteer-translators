314a9dc5fce2b345ee3bc956b2ba5534b6abb446
HellLena <longlivehard.n.heavy@gmail.com>
2016-11-07 19:00:12 +0500

    #5: finish VK auth

--- application/controllers/user/auth.php
+++ application/controllers/user/auth.php
@@ -34,73 +34,20 @@ class Auth extends My_Controller {
 					if ($auther->authenticate()) {
 						$user = $this->users_model->socialUserExists($auther->getSocialId(),$provider);
 						if(empty($user)){
-							if(!$this->users_model->createSocialUser($auther)){
-								// show error
+							$user_id = $this->users_model->createSocialUser($auther);
+							if(empty($user_id)){
+								$data['msg'] = '<div class="alert alert-danger text-center">Приносим извинения. Произошла ошибка при записи данных. Попробуйте войти позже.</div>';
 							}
+						} else {
+							$user_id = $user->id;
 						}
-//						if($this->ion_auth->loginSocialUser($user->id)){
-//							redirect('user/profile');
-//						}
-
-//					$result = mysql_query(
-//						"SELECT *  FROM `users` WHERE `provider` = '{$auther->getProvider()}' AND `social_id` = '{$auther->getSocialId()}' LIMIT 1"
-//					);
-//
-//					$record = mysql_fetch_array($result);
-//					if (!$record) {
-//						$values = array(
-//							$auther->getProvider(),
-//							$auther->getSocialId(),
-//							$auther->getName(),
-//							$auther->getEmail(),
-//							$auther->getSocialPage(),
-//							$auther->getSex(),
-//							date('Y-m-d', strtotime($auther->getBirthday())),
-//							$auther->getAvatar()
-//						);
-//
-//						$query = "INSERT INTO `users` (`provider`, `social_id`, `name`, `email`, `social_page`, `sex`, `birthday`, `avatar`) VALUES ('";
-//						$query .= implode("', '", $values) . "')";
-//						$result = mysql_query($query);
-//					} else {
-//						$userFromDb = new stdClass();
-//						$userFromDb->provider   = $record['provider'];
-//						$userFromDb->socialId   = $record['social_id'];
-//						$userFromDb->name       = $record['name'];
-//						$userFromDb->email      = $record['email'];
-//						$userFromDb->socialPage = $record['social_page'];
-//						$userFromDb->sex        = $record['sex'];
-//						$userFromDb->birthday   = date('m.d.Y', strtotime($record['birthday']));
-//						$userFromDb->avatar     = $record['avatar'];
-//					}
-//
-//					$user = new stdClass();
-//					$user->provider   = $auther->getProvider();
-//					$user->socialId   = $auther->getSocialId();
-//					$user->name       = $auther->getName();
-//					$user->email      = $auther->getEmail();
-//					$user->socialPage = $auther->getSocialPage();
-//					$user->sex        = $auther->getSex();
-//					$user->birthday   = $auther->getBirthday();
-//					$user->avatar     = $auther->getAvatar();
-//
-//					if (isset($userFromDb) && $userFromDb != $user) {
-//						$idToUpdate = $record['id'];
-//						$birthday = date('Y-m-d', strtotime($user->birthday));
-//
-//						mysql_query(
-//							"UPDATE `users` SET " .
-//							"`social_id` = '{$user->socialId}', `name` = '{$user->name}', `email` = '{$user->email}', " .
-//							"`social_page` = '{$user->socialPage}', `sex` = '{$user->sex}', " .
-//							"`birthday` = '{$birthday}', `avatar` = '$user->avatar' " .
-//							"WHERE `id`='{$idToUpdate}'"
-//						);
-//					}
-//
-//					$_SESSION['user'] = $user;
+						if($this->ion_auth->loginSocialUser($user_id)){
+							$this->session->set_flashdata('msg', $this->ion_auth->messages());
+							redirect('user/profile');
+						} else {
+							$data['msg'] = $this->ion_auth->errors();
+						}
 					}
-
-					echo "not";
 				}
 			} else if($this->input->post('do_login')
 				&& $this->form_validation->run()
