1359e53636c63a682ab209cdedd529295220c57d
HellLena <longlivehard.n.heavy@gmail.com>
2016-11-22 14:49:29 +0500

    #10: fix register form

--- application/controllers/user/register.php
+++ application/controllers/user/register.php
@@ -139,7 +139,8 @@ class Register extends MY_Form
 
     function activate(){
         $params = $this->input->get();
-        $reset = $this->security->xss_clean($params["reset"]);
+        $reset = false;
+        if($params["reset"]) $reset = $this->security->xss_clean($params["reset"]);
         if($this->users_model->verifyEmailID($params)){
             if($reset == true && $this->ion_auth->logged_in()) {
                 $this->session->set_flashdata('msg','<div class="alert alert-success text-center">Ваш электронный адресс успешно подтверждён.</div>');
